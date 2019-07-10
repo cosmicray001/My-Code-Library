7df5c6584f4a5fb145b08778dfa46a2b0fbcd9c8
cosmicray001 <32748530+cosmicray001@users.noreply.github.com>
2019-07-10 07:26:09 +0600

    Update big_int.cpp

--- Number-Theory/big_int.cpp
+++ Number-Theory/big_int.cpp
@@ -98,13 +98,100 @@ struct Bigint {
         for( int i = a.size() - 1; i >= 0; i-- ) putchar(a[i]);
     }
 };
-int main(){
-  Bigint a, b;
-  string s;
-  cin >> s;
-  a = s;
-  cin >> s;
-  b = s;
-  a = a * b;
-  a.print();
+int main() {
+
+    Bigint a, b, c; // declared some Bigint variables
+
+ 
+
+    /////////////////////////
+
+    // taking Bigint input //
+
+    /////////////////////////
+
+    string input; // string to take input
+
+ 
+
+    cin >> input; // take the Big integer as string
+
+    a = input; // assign the string to Bigint a
+
+ 
+
+    cin >> input; // take the Big integer as string
+
+    b = input; // assign the string to Bigint b
+
+   
+
+    //////////////////////////////////
+
+    // Using mathematical operators //
+
+    //////////////////////////////////
+
+ 
+
+    c = a + b; // adding a and b
+
+    c.print(); // printing the Bigint
+
+    puts(""); // newline
+
+ 
+
+    c = a - b; // subtracting b from a
+
+    c.print(); // printing the Bigint
+
+    puts(""); // newline
+
+ 
+
+    c = a * b; // multiplying a and b
+
+    c.print(); // printing the Bigint
+
+    puts(""); // newline
+
+ 
+
+    c = a / b; // dividing a by b
+
+    c.print(); // printing the Bigint
+
+    puts(""); // newline
+
+ 
+
+    c = a % b; // a modulo b
+
+    c.print(); // printing the Bigint
+
+    puts(""); // newline
+
+ 
+
+    /////////////////////////////////
+
+    // Using conditional operators //
+
+    /////////////////////////////////
+
+ 
+
+    if( a == b ) puts("equal"); // checking equality
+
+    else puts("not equal");
+
+ 
+
+    if( a < b ) puts("a is smaller than b"); // checking less than operator
+
+ 
+
+    return 0;
+
 }
